358a65b0b817893028c1475b8fbdf3eb0491285e
mtackes <michaeltackes@gmail.com>
2015-02-18 22:38:42 -0600

    Initial Commit

---
+++ DollarWords/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DollarWords
+//
+//  Created by Michael Tackes on 2/18/15.
+//  Copyright (c) 2015 Michael Tackes. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
